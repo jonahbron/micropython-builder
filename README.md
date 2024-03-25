60f0124cf6f7a64772eac79cc87340ddefe846bd
Jonah Bron <hi@jonah.id>
2024-03-25 05:53:57 +0000

    Updated examples and readme with new interface

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # MicroPython Builder
 
-This is a flake that knows how to build MicroPython.  It can either be invoked
-directly to build a basic firmware, or it can be used as an input flake and
-be used as a tool to build a custom firmware. 
+This is a flake that knows how to build and flash MicroPython.  It can either
+be invoked directly to build a basic firmware, or it can be used as an input
+flake and be used as a tool to build a custom firmware.
 
 ```bash
-nix run github:jonahbron/micropython-builder#flash-esp32GenericC3
+nix run github:jonahbron/micropython-builder#flash-esp32c3-generic
 ```
 
 See [`examples/`](./examples) for examples of usage as an input to another
@@ -21,18 +21,20 @@ nix build ./examples#
   - [x] [ESP32_GENERIC_C3](https://github.com/micropython/micropython/tree/master/ports/esp32/boards/ESP32_GENERIC_C3)
 
 ## TODO
-- [ ] Flake package that can be run like `nix build .#esp32GenericC3`
+- [ ] Flake package that can be run like `nix build .#esp32c3-generic`
   - [ ] Default output is firmware.bin
   - [ ] Other output portions can be accessed too `nix build .#esp32GenericC3.bootloader`
-- [ ] Flake package that can be run like `nix run .#flash-esp32GenericC3`
-- [x] Top-level Flake output callable named like buildMicroPythonFirmware
+- [ ] Flake package that can be run like `nix run .#flash-esp32-generic`
+- [x] Flake library that can be called like `buildMicroPythonFirmware`
   - [x] Allows specifying the port/board
   - [x] Accepts arguments pointing to manifest.py
   - [x] Accepts arguments pointing to user_c_modules
   - [x] Allow patch phase script
   - [ ] Allow overriding the micropython source
   - [ ] Allow overriding the micropython-lib source
-- [ ] Make interface more ergonomic, require less knowledge
+- [x] Flake library that can be called like `flashMicroPythonFirmware`
+- [x] Make interface more ergonomic, require less knowledge
+- [ ] Factor out common ESP32 expressions.
 - [ ] Add nix formatter
 - [ ] Flake template
 - [ ] Contribute into nixpkgs?
--- examples/flake.nix
+++ examples/flake.nix
@@ -2,18 +2,14 @@
   description = "Example building flake";
   inputs.micropython-builder.url = "github:jonahbron/micropython-builder";
   outputs = { self, micropython-builder }: {
-    packages.x86_64-linux.default = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
-      port = "esp32";
-      board = "ESP32_GENERIC_C3";
+    packages.x86_64-linux.default = micropython-builder.lib.x86_64-linux.esp32c3.buildMicroPythonFirmware {
       sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
     };
 
     # This approach is for the purpose of the example, but it's not ideal
     # because the dependency version cannot be frozen. It's recommended to
     # instead call freeze() on a derivation pulled in with fetchFromGitHub.
-    packages.x86_64-linux.withAIoHttp = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
-      port = "esp32";
-      board = "ESP32_GENERIC_C3";
+    packages.x86_64-linux.frozenManifest = micropython-builder.lib.x86_64-linux.esp32c3.buildMicroPythonFirmware {
       sha256 = "sha256-XTQq1dwpj/lDAxzH/cQnbRF1zljvCMRvaXd9He+YJNY=";
       frozenManifestText = ''
         require('aiohttp')
@@ -23,9 +19,7 @@
     # Point userCModules to a derivation containing all of the C code that the
     # firmware needs to have.  It must have a `micropython.cmake` file at the
     # root that includes everything.
-    packages.x86_64-linux.withUserCModules = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
-      port = "esp32";
-      board = "ESP32_GENERIC_C3";
+    packages.x86_64-linux.userCModules = micropython-builder.lib.x86_64-linux.esp32c3.buildMicroPythonFirmware {
       userCModules = ./user_c_modules;
       sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
     };
@@ -33,9 +27,7 @@
     # PatchPhase current directory is in the build environment, in the root of
     # the micropython source.  Here arbitrary scripts can be run to mutate the
     # source or libraries before build.
-    packages.x86_64-linux.withPatchPhase = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
-      port = "esp32";
-      board = "ESP32_GENERIC_C3";
+    packages.x86_64-linux.patchPhase = micropython-builder.lib.x86_64-linux.esp32c3.buildMicroPythonFirmware {
       patchPhase = ''
         echo 'Hello world' > text.txt
       '';
@@ -44,10 +36,8 @@
 
     # Derivation's output contains an executable that can be run with
     # `nix run .#flash`
-    packages.x86_64-linux.flash = micropython-builder.lib.x86_64-linux.flashMicroPythonFirmware {
-      port = "esp32";
-      board = "ESP32_GENERIC_C3";
+    packages.x86_64-linux.flash = micropython-builder.lib.x86_64-linux.esp32c3.flashMicroPythonFirmware {
       sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
-    } { chip = "esp32c3"; };
+    };
   };
 }
