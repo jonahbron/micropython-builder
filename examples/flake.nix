d999823f2a58a9ed94009e8ba4ad83d9dde2428a
Jonah Bron <hi@jonah.id>
2024-03-25 04:26:13 +0000

    Added extremely minimal example for including user C modules

--- examples/flake.nix
+++ examples/flake.nix
@@ -8,8 +8,9 @@
       sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
     };
 
-    # This approach is fine but it is not purely repeatable, as it always pulls
-    # in the latest version.  It is better to use freeze() on a derivation.
+    # This approach is for the purpose of the example, but it's not ideal
+    # because the dependency version cannot be frozen. It's recommended to
+    # instead call freeze() on a derivation pulled in with fetchFromGitHub.
     packages.x86_64-linux.withAIoHttp = micropython-builder.lib.x86_64-linux.mkFirmwareDerivation {
       port = "esp32";
       board = "ESP32_GENERIC_C3";
@@ -17,7 +18,16 @@
       frozenManifestText = ''
         require('aiohttp')
       '';
-      # userCModules = ./derivation-containing-cmake
+    };
+
+    # Point userCModules to a derivation containing all of the C code that the
+    # firmware needs to have.  It must have a `micropython.cmake` file at the
+    # root that includes everything.
+    packages.x86_64-linux.withUserCModules = micropython-builder.lib.x86_64-linux.mkFirmwareDerivation {
+      port = "esp32";
+      board = "ESP32_GENERIC_C3";
+      userCModules = ./user_c_modules;
+      sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
     };
   };
 }
