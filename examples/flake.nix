dd691bda6cf72f1631ea52a4a7e15338eba3244c
Jonah Bron <hi@jonah.id>
2024-03-25 05:15:05 +0000

    Added flash function to examples

--- examples/flake.nix
+++ examples/flake.nix
@@ -2,7 +2,7 @@
   description = "Example building flake";
   inputs.micropython-builder.url = "github:jonahbron/micropython-builder";
   outputs = { self, micropython-builder }: {
-    packages.x86_64-linux.default = micropython-builder.lib.x86_64-linux.mkFirmwareDerivation {
+    packages.x86_64-linux.default = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
       port = "esp32";
       board = "ESP32_GENERIC_C3";
       sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
@@ -11,7 +11,7 @@
     # This approach is for the purpose of the example, but it's not ideal
     # because the dependency version cannot be frozen. It's recommended to
     # instead call freeze() on a derivation pulled in with fetchFromGitHub.
-    packages.x86_64-linux.withAIoHttp = micropython-builder.lib.x86_64-linux.mkFirmwareDerivation {
+    packages.x86_64-linux.withAIoHttp = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
       port = "esp32";
       board = "ESP32_GENERIC_C3";
       sha256 = "sha256-XTQq1dwpj/lDAxzH/cQnbRF1zljvCMRvaXd9He+YJNY=";
@@ -23,7 +23,7 @@
     # Point userCModules to a derivation containing all of the C code that the
     # firmware needs to have.  It must have a `micropython.cmake` file at the
     # root that includes everything.
-    packages.x86_64-linux.withUserCModules = micropython-builder.lib.x86_64-linux.mkFirmwareDerivation {
+    packages.x86_64-linux.withUserCModules = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
       port = "esp32";
       board = "ESP32_GENERIC_C3";
       userCModules = ./user_c_modules;
@@ -33,7 +33,7 @@
     # PatchPhase current directory is in the build environment, in the root of
     # the micropython source.  Here arbitrary scripts can be run to mutate the
     # source or libraries before build.
-    packages.x86_64-linux.withPatchPhase = micropython-builder.lib.x86_64-linux.mkFirmwareDerivation {
+    packages.x86_64-linux.withPatchPhase = micropython-builder.lib.x86_64-linux.buildMicroPythonFirmware {
       port = "esp32";
       board = "ESP32_GENERIC_C3";
       patchPhase = ''
@@ -41,5 +41,13 @@
       '';
       sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
     };
+
+    # Derivation's output contains an executable that can be run with
+    # `nix run .#flash`
+    packages.x86_64-linux.flash = micropython-builder.lib.x86_64-linux.flashMicroPythonFirmware {
+      port = "esp32";
+      board = "ESP32_GENERIC_C3";
+      sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
+    } { chip = "esp32c3"; };
   };
 }
