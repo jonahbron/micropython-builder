580e8e2d7f5e4b5dcafd3555a708e8a8c190315b
Jonah Bron <hi@jonah.id>
2024-03-25 03:39:51 +0000

    Added example for freezing in a Python library from micropython-lib

--- examples/flake.nix
+++ examples/flake.nix
@@ -6,7 +6,17 @@
       port = "esp32";
       board = "ESP32_GENERIC_C3";
       sha256 = "sha256-pgp40inNjT1cCX2S8/EjVTTytaK7zz8+x1sT7nWLHT0=";
-      # freezeManifest = ./manifest.py;
+    };
+
+    # This approach is fine but it is not purely repeatable, as it always pulls
+    # in the latest version.  It is better to use freeze() on a derivation.
+    packages.x86_64-linux.withAIoHttp = micropython-builder.lib.x86_64-linux.mkFirmwareDerivation {
+      port = "esp32";
+      board = "ESP32_GENERIC_C3";
+      sha256 = "sha256-XTQq1dwpj/lDAxzH/cQnbRF1zljvCMRvaXd9He+YJNY=";
+      frozenManifestText = ''
+        require('aiohttp')
+      '';
       # userCModules = ./derivation-containing-cmake
     };
   };
